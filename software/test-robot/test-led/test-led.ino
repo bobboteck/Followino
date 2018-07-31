bc43c3890026b8b5b3248993ba5acaf427f0a985
mauro-vaio\mauro <mauro.espo@gmail.com>
2018-07-31 23:27:23 +0200

    ADD: aggiunto codice test led status 1 (D4)/status 2 (d9)

--- software/test-robot/test-led/test-led.ino
+++ software/test-robot/test-led/test-led.ino
@@ -1,10 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// D4: led status 1
+// D9: led status 2
+
+int ledPin = 4;
+int ledPin_red = 9;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin_red, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin_red, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin_red, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);       
 }
-
