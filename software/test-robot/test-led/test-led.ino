b1c0f7ab237e0216a4a6f68da1afe14567a40a1b
mauro-vaio\mauro <mauro.espo@gmail.com>
2018-08-01 00:16:15 +0200

    FIX: sistemato codice test led.

--- software/test-robot/test-led/test-led.ino
+++ software/test-robot/test-led/test-led.ino
@@ -1,20 +1,26 @@
+/*
+  test-led
 
-// D4: led status 1
-// D9: led status 2
+  Semplice script di prova dei led di status collegati ai pin D4 (Status 1, red) e D9 (Status 2, green).
 
-int ledPin = 4;
-int ledPin_red = 9;
+  Created 01 08 2018
+  By Mauro Esposito / Roberto D'Amico
+
+*/
+
+int led_status_1 = 4;
+int led_status_2 = 9;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(ledPin_red, OUTPUT);
+  pinMode(led_status_1, OUTPUT);
+  pinMode(led_status_2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin_red, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(ledPin_red, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);       
+  digitalWrite(led_status_1, HIGH);   // status 1 acceso
+  digitalWrite(led_status_2, LOW);    // status 2 spento
+  delay(1000);                        // pausa di un secondo
+  digitalWrite(led_status_1, LOW);    // status 1 spento
+  digitalWrite(led_status_2, HIGH);   // status 2 acceso
+  delay(1000);                        // pausa di un secondo
 }
